2c8b6ae6cbc06007b59906dac63fa34797d2512f
pio-o-connell <pio_o_con@hotmail.com>
2016-03-19 08:09:15 +0000

    Third Java app created - back in 2013 Now Beautified.

--- src/thirdjava/MovieCatalog.java
+++ src/thirdjava/MovieCatalog.java
@@ -14,11 +14,10 @@ package thirdjava;
  *
  * @author Pio O'Connell
  */
-
-
 import java.util.*;
 
 import java.util.ArrayList;
+
 public class MovieCatalog {
 
     /**
@@ -26,111 +25,98 @@ public class MovieCatalog {
      */
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-	ArrayList<String> films = new ArrayList<String>(); //
-	ArrayList<films> film = new ArrayList<films>(); //Temporary buffer to read in the movies for an actor
-	ArrayList<actors> actor = new ArrayList<actors>(); // Main structure that contains all the actor details
-	films test;
-	String tempName,tempAddress; // temporary varibles to hold the names,addresses
-	String tempAge; // tempory veriable to hold the age
-	int tempAge1=0 /*,nmrActors=0 */; // tempory variable to hold the converted String from io
-	Boolean numeric=true; // whether io numerical
-	actors tempactor; // temporary variable to hold the actor
-	char input;
-	Boolean actorfinished=false; // set to false whilst continuing to read new actors
+        ArrayList<String> films = new ArrayList<String>(); //
+        ArrayList<films> film = new ArrayList<films>(); //Temporary buffer to read in the movies for an actor
+        ArrayList<actors> actor = new ArrayList<actors>(); // Main structure that contains all the actor details
+        films test;
+        String tempName, tempAddress; // temporary varibles to hold the names,addresses
+        String tempAge; // tempory veriable to hold the age
+        int tempAge1 = 0 /*,nmrActors=0 */; // tempory variable to hold the converted String from io
+        Boolean numeric = true; // whether io numerical
+        actors tempactor; // temporary variable to hold the actor
+        char input;
+        Boolean actorfinished = false; // set to false whilst continuing to read new actors
 
-	do
-	{
-			System.out.println("------------------------------------");
-			System.out.println("Actor's movie catalog system");
-			System.out.println("Add/View/Delete actor's film history :");
-			System.out.println("------------------------------------");
-		
-			System.out.println("Enter Actor's Name:(e.g. John Wayne) " );
-			tempName = keyboard.nextLine();
-			System.out.println("Enter Actor's Address:");
-			tempAddress = keyboard.nextLine();
-			System.out.println("Enter Actor's age :");
-			tempAge = keyboard.nextLine();
-			try {
-				tempAge1=Integer.parseInt(tempAge);  
-			} catch (NumberFormatException e) {
-		      //Will Throw exception!
-		      //do something! anything to handle the exception.
-			numeric = false;				
+        do {
+            System.out.println("------------------------------------");
+            System.out.println("Actor's movie catalog system");
+            System.out.println("Add/View/Delete actor's film history :");
+            System.out.println("------------------------------------");
 
-			System.out.print("Invalid -- no records will be added");
-			}	
-		/*	if(numeric){
+            System.out.println("Enter Actor's Name:(e.g. John Wayne) ");
+            tempName = keyboard.nextLine();
+            System.out.println("Enter Actor's Address:");
+            tempAddress = keyboard.nextLine();
+            System.out.println("Enter Actor's age :");
+            tempAge = keyboard.nextLine();
+            try {
+                tempAge1 = Integer.parseInt(tempAge);
+            } catch (NumberFormatException e) {
+                //Will Throw exception!
+                //do something! anything to handle the exception.
+                numeric = false;
+
+                System.out.print("Invalid -- no records will be added");
+            }
+            /*	if(numeric){
 				nmrActors++;		
 			}*/
-		
+
 //			String code=" ";
-			String temp=" ";
-			Boolean moviefinished =false;
-		
-			do
-			{
-				System.out.println("Please enter Movie Name into  "+tempName+" 's Catalog  :");
-		
-				// reads the movie name from input
-				temp = keyboard.nextLine();
-				// adds to film
-				films.add(temp);
-				// create new film structure and add to film array
-				test = new films(temp);
-				film.add(test);
+            String temp = " ";
+            Boolean moviefinished = false;
 
-				// display the details entered
-				
-				System.out.println(film);
-				System.out.println("Enter another('y'/'n')  :");
-				input = keyboard.next().trim().charAt(0);
-				if (input == 'y'){	
+            do {
+                System.out.println("Please enter Movie Name into  " + tempName + " 's Catalog  :");
+
+                // reads the movie name from input
+                temp = keyboard.nextLine();
+                // adds to film
+                films.add(temp);
+                // create new film structure and add to film array
+                test = new films(temp);
+                film.add(test);
+
+                // display the details entered
+                System.out.println(film);
+                System.out.println("Enter another('y'/'n')  :");
+                input = keyboard.next().trim().charAt(0);
+                if (input == 'y') {
 //					code = " ";
-					moviefinished = false;
-					keyboard.nextLine(); // to fflush() the buffer	
-				}
-				else if (input == 'n'){
-					moviefinished = true;			
-				}
-				else
-				{
-					System.out.println("Invalid input");
-					moviefinished = true;
-				}			
-			}
-			while (!moviefinished);
-		
-		// create new actor instance and add to the actor array
-		tempactor = new actors(tempName,tempAddress,tempAge1,film);
-		//allocate new memory for new film instance,last memory now part of previous actors
-		film=new ArrayList<films>();
-		actor.add(tempactor);
-		System.out.println(" Movie stars with their movies entered into temporary database");
-		System.out.println(actor);
+                    moviefinished = false;
+                    keyboard.nextLine(); // to fflush() the buffer	
+                } else if (input == 'n') {
+                    moviefinished = true;
+                } else {
+                    System.out.println("Invalid input");
+                    moviefinished = true;
+                }
+            } while (!moviefinished);
 
-		System.out.println("Enter another actor's details('y'/'n')  :");
-		input = keyboard.next().trim().charAt(0);
-		if (input == 'y'){	
+            // create new actor instance and add to the actor array
+            tempactor = new actors(tempName, tempAddress, tempAge1, film);
+            //allocate new memory for new film instance,last memory now part of previous actors
+            film = new ArrayList<films>();
+            actor.add(tempactor);
+            System.out.println(" Movie stars with their movies entered into temporary database");
+            System.out.println(actor);
+
+            System.out.println("Enter another actor's details('y'/'n')  :");
+            input = keyboard.next().trim().charAt(0);
+            if (input == 'y') {
 //			code = " ";
-			actorfinished = false;
-			keyboard.nextLine(); // to fflush() the buffer	
-		}
-		else if (input == 'n'){
-			actorfinished = true;			
-		}
-		else
-		{
-			System.out.println("Invalid input -- try again");
-			actorfinished = false;
-		}	
+                actorfinished = false;
+                keyboard.nextLine(); // to fflush() the buffer	
+            } else if (input == 'n') {
+                actorfinished = true;
+            } else {
+                System.out.println("Invalid input -- try again");
+                actorfinished = false;
+            }
 
-		}
-		while (!actorfinished);
-		System.out.println(" Movie stars with their movies entered into temporary database");
-		System.out.println(actor);
-		System.out.println("Goodbye  -u have terminated peacefully");
-		}
+        } while (!actorfinished);
+        System.out.println(" Movie stars with their movies entered into temporary database");
+        System.out.println(actor);
+        System.out.println("Goodbye  -u have terminated peacefully");
     }
-    
-
+}
--- src/thirdjava/films.java
+++ src/thirdjava/films.java
@@ -10,35 +10,34 @@ package thirdjava;
  * @author user
  */
 public class films {
-    private String Name;
-    private String Code =" ";
-	
-    public films (String Name0){
-		this.Name = Name0;
-		
-		for(String word:(Name.split (" "))){
-			Code += word.charAt(0);
-		}
-		
-	}
 
-	public void setName(String Name0){
-		this.Name = Name0;
-	}
-	
-	public String getName(){
-		return Name;
-	}
-	
-	public String getCode(){
-		return Code;
-	}
-	
-	
-	
-public String toString (){
-		
-		return( this.Name +" \t\t");	
-		// to get proper formatting,it would be better to read into fixed width table first and display
-	}
+    private String Name;
+    private String Code = " ";
+
+    public films(String Name0) {
+        this.Name = Name0;
+
+        for (String word : (Name.split(" "))) {
+            Code += word.charAt(0);
+        }
+
+    }
+
+    public void setName(String Name0) {
+        this.Name = Name0;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public String toString() {
+
+        return (this.Name + " \t\t");
+        // to get proper formatting,it would be better to read into fixed width table first and display
+    }
 }
